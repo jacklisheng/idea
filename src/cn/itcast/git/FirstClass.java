b074b834987d0fb4b3c61da3d5b1da09c5ffd1c7
jackren <478421880@qq.com>
2018-12-16 19:59:23 +0800

    修改中

---
+++ src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,13 @@
+package cn.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+
+    }
+}
